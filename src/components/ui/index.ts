af3570766ec4dea5c270907d387ff5f1cef6d377
Amir <amirphl4@gmail.com>
2025-06-02 14:34:01 +0000

    chore: export UI components from index file
    - Button
    - Card
    - FormField
    - StepHeader
    - Stepper

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+// UI Components Index
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as FormField } from './FormField';
+export { default as StepHeader } from './StepHeader';
+export { default as Stepper } from './Stepper'; 
