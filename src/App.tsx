20c66a85057b6d1bb2b1e702a1df562f3b8e03fe
Amir <amirphl4@gmail.com>
2026-02-06 16:43:30 +0000

    Bypass global chrome when rendering landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AuthRouter from './components/AuthRouter';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
+import { ROUTES } from './config/routes';
 import { LanguageProvider } from './hooks/useLanguage';
 import { ToastProvider } from './components/ToastContainer';
 import { AuthProvider } from './hooks/useAuth';
@@ -11,23 +12,31 @@ import { CampaignProvider } from './hooks/useCampaign';
 import { NavigationProvider } from './contexts/NavigationContext';
 
 function App() {
+  const isLandingPage =
+    typeof window !== 'undefined' &&
+    window.location.pathname === ROUTES.LANDING.path;
+
   return (
     <LanguageProvider>
       <AuthProvider>
         <CampaignProvider>
           <NavigationProvider>
             <ToastProvider>
-              <div className='min-h-screen bg-gray-50 flex flex-col'>
-                <Header />
-                <div className='flex-1 flex'>
-                  {/* Global sidebar shown on all pages */}
-                  <Sidebar />
-                  <main className='flex-1'>
-                    <AuthRouter />
-                  </main>
+              {isLandingPage ? (
+                <AuthRouter />
+              ) : (
+                <div className='min-h-screen bg-gray-50 flex flex-col'>
+                  <Header />
+                  <div className='flex-1 flex'>
+                    {/* Global sidebar shown on all pages */}
+                    <Sidebar />
+                    <main className='flex-1'>
+                      <AuthRouter />
+                    </main>
+                  </div>
+                  <Footer />
                 </div>
-                <Footer />
-              </div>
+              )}
             </ToastProvider>
           </NavigationProvider>
         </CampaignProvider>
