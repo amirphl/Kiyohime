71fca73acb6749f29729f5794127da342cdfdcc2
Amir <amirphl4@gmail.com>
2026-02-22 12:54:56 +0000

    Export signup component set

---
+++ src/pages/signup/components/index.ts
@@ -0,0 +1,8 @@
+export { default as AccountTypeSelect } from './AccountTypeSelect';
+export { default as CompanyFields } from './CompanyFields';
+export { default as CategoryJobFields } from '../../../components/CategoryJobFields';
+export { default as PersonalInfoFields } from './PersonalInfoFields';
+export { default as CredentialsFields } from './CredentialsFields';
+export { default as AgencyCodeField } from './AgencyCodeField';
+export { default as TermsCheckbox } from './TermsCheckbox';
+export { default as OtpModal } from './OtpModal';
