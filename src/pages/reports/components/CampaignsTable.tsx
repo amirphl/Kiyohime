1466adcc3db9294698a32f2423e92b7cafe5b7d1
Amir <amirphl4@gmail.com>
2026-03-08 20:20:33 +0000

    Show campaign title in reports table

--- src/pages/reports/components/CampaignsTable.tsx
+++ src/pages/reports/components/CampaignsTable.tsx
@@ -70,7 +70,7 @@ const CampaignsTable: React.FC<CampaignsTableProps> = ({
                     {idx + 1}
                   </td>
                   <td className='px-4 py-2 text-sm text-gray-700 text-center'>
-                    {truncateText(c.content || '')}
+                    {truncateText(c.title || '')}
                   </td>
                   <td className='px-4 py-2 text-sm text-gray-900 text-center'>
                     {c.line_number || '-'}
