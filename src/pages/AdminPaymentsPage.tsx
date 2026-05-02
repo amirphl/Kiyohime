a0c182d9dfe5ea7f9d3d7e1b44300367f20bc26a
amirphl <amirphl4@gmail.com>
2026-05-02 13:00:00 +0330

    Enhance admin payments page structure and flow

--- src/pages/AdminPaymentsPage.tsx
+++ src/pages/AdminPaymentsPage.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdminChargeWalletSection from './adminPayments/components/AdminChargeWalletSection';
 import AdminDepositReceiptsSection from './adminPayments/components/AdminDepositReceiptsSection';
+import AdminTransactionsSection from './adminPayments/components/AdminTransactionsSection';
 
 const AdminPaymentsPage: React.FC = () => {
+  const [transactionsRefreshKey, setTransactionsRefreshKey] = useState(0);
+
   return (
     <div className='p-4'>
       <div className='max-w-5xl mx-auto space-y-4'>
         <AdminChargeWalletSection />
-        <AdminDepositReceiptsSection />
+        <AdminDepositReceiptsSection
+          onReceiptStatusUpdated={() =>
+            setTransactionsRefreshKey(prev => prev + 1)
+          }
+        />
+        <AdminTransactionsSection refreshSignal={transactionsRefreshKey} />
       </div>
     </div>
   );
