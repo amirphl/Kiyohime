6b3615abfbf575439772c8603885f6ecd2609a32
amirphl <amirphl4@gmail.com>
2026-05-05 22:46:01 +0330

    refactor(admin-campaigns): export new management hooks

--- src/pages/adminCampaignManagement/index.ts
+++ src/pages/adminCampaignManagement/index.ts
@@ -1,2 +1,6 @@
 export { adminCampaignManagementApi } from './api';
 export { getAdminCampaignManagementCopy } from './translations';
+export { useCampaignFilters } from './hooks/useCampaignFilters';
+export { useCampaignList } from './hooks/useCampaignList';
+export { useCampaignActions } from './hooks/useCampaignActions';
+export { useCampaignReschedule } from './hooks/useCampaignReschedule';
